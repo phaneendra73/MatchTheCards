9239fd940b5aaf44264f23cf99a148ea9a225689
phaneendra73 <phaneendra3377@gmail.com>
2023-04-19 21:19:02 +0530

    almost completed

--- script.js
+++ script.js
@@ -26,12 +26,99 @@ let cardsArray = [
     }
 ];
 
+
 const parentDiv = document.querySelector('#card-section');
 const gamecards = cardsArray.concat(cardsArray)
-for (let i = 0; i < gamecards.length; i++) {
+
+const random = (gamecards) => {
+    for (i = 0; i < gamecards.length; i++) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let temp = gamecards[i]
+        gamecards[i] = gamecards[j]
+        gamecards[j] = temp
+    }
+    return gamecards
+}
+const shuffledcards = random(gamecards);
+
+let clickcount = 0;
+let firstcard = "";
+let secondcard = "";
+const card_matches = () => {
+    let card_selected = document.querySelectorAll('.card_selected');
+    card_selected.forEach((curElem) => {
+        curElem.classList.add('card_match');
+    });
+}
+let resetGame = () => {
+    clickcount = 0;
+    firstcard = "";
+    secondcard ="";
+    let card_selected = document.querySelectorAll('.card_selected');
+    card_selected.forEach((curElem) => {
+        curElem.classList.remove('card_selected')
+
+})
+}
+
+
+parentDiv.addEventListener('click', (event) => {
+    let current_card = event.target;
+    if(current_card.id === "card-section"){return false }
+    clickcount++;
+
+    if (clickcount < 3) {
+        if (clickcount === 1) {
+            firstcard = current_card.parentNode.dataset.name;
+            console.log(firstcard)
+            current_card.parentNode.classList.add('card_selected');
+
+
+        }
+        else {
+            secondcard = current_card.parentNode.dataset.name;
+            console.log(secondcard)
+            current_card.parentNode.classList.add('card_selected');
+        }
+
+        if (firstcard !=="" && secondcard !=="") {
+            if (firstcard === secondcard) {
+                setTimeout(() => {
+                card_matches()
+                resetGame()
+                    
+                }, 1000);
+                
+
+                current_card.id == 'card-section' ? null : current_card.classList.add('card_selected')
+            }
+            else{
+                setTimeout(() => {
+                    resetGame()
+                        
+                    }, 1000)
+            }
+
+        }
+    }
+
+})
+
+for (let i = 0; i < shuffledcards.length; i++) {
+
     const childDiv = document.createElement('div')
     childDiv.classList.add('card')
-    childDiv.dataset.name = gamecards[i].name;
-    childDiv.style.backgroundImage = `url(${gamecards[i].img})`;
+    childDiv.dataset.name = shuffledcards[i].name;
+    // childDiv.style.backgroundImage = `url(${shuffledcards[i].img})`;
+
+    const front_div=document.createElement('div')
+    front_div.classList.add('front-card')
+
+    const back_div=document.createElement('div')
+    back_div.classList.add('back-card')
+    back_div.style.backgroundImage = `url(${shuffledcards[i].img})`;
+
     parentDiv.appendChild(childDiv)
+    childDiv.appendChild(front_div)
+    childDiv.appendChild(back_div)
 }
